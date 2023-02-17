42f6575eb5f55ea59a1f222a7331ddd88ab134f1
Valery Maltsev <lisowsky@yandex.ru>
2023-02-17 18:06:22 +0300

    Task 1 - 2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 public class Main {
+    // Объявить метод «сгенерироватьМассив»
+
     public static void main(String[] args) {
         task1();
         task2();
@@ -8,18 +10,46 @@ public class Main {
         task6();
         task7();
         task8();
-
     }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+
     public static void task1() {
         System.out.println("Задача 1");
         // Пишем код для задачи 1
+        int [] arr = generateRandomArray();
+        int summ = 0;
+        for (int j : arr) {
+            summ = summ + j;
+        }
+        System.out.println(summ);
 
     }
 
     public static void task2() {
         System.out.println("Задача 2");
         // Пишем код для задачи 2
+        int [] arr = generateRandomArray();
+        int max = 0;
+        int min = 200_001;
 
+        for (final int current : arr) {
+            if (max < current) {
+                max = current;
+            } else if (min > current) {
+                min = current;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " +min+ " рублей.");
+        System.out.println("Максимальная сумма трат за день составила " +max+ " рублей.");
     }
 
     public static void task3() {
